f3f68f5ae19441adbaafcf53ffeff6045a8f0e4e
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-20 16:54:35 -0300

    Create Day00.py

---
+++ Day00.py
@@ -0,0 +1,8 @@
+#Day 0
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+# Print a string literal saying "Hello, World." to stdout.
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+
+input_string = input()
+print('Hello, World.')
