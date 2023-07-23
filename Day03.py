ef6455363f9d1b36fe3cc048d60269a2fb58e9ca
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-23 15:27:45 -0300

    Create Day03.py
    Day 3 - Conditionals

---
+++ Day03.py
@@ -0,0 +1,23 @@
+#Day 3 - Conditionals
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N % 2 == 0 and (N >= 2 and N <= 5):
+        print('Not Weird')
+    elif N % 2 == 0 and (N <= 6 and N >= 20):
+        print('Weird')
+    elif N % 2 == 0 and N > 20:
+        print('Not Weird')
+    else:
+        print('Weird')
+        
