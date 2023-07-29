58f770db5967156b1b8fde3de33ff4a2a1e7a5e1
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-07-28 21:31:15 -0300

    Create Day07.py
    Arrays

---
+++ Day07.py
@@ -0,0 +1,19 @@
+#Day 07 - Arrays
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    for i in reversed(arr):
+      print(i, end=' ')
